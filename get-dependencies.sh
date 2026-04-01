965b5817290a34a05cc815f2386420816f0a5cd1
Link4Electronics <link4electronics@gmail.com>
2026-04-01 01:05:54 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,18 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    python         \
+    vkrunner       \
+    vulkan-headers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package waffle
+make-aur-package piglit-git
 
 # If the application needs to be manually built that has to be done down here
 
